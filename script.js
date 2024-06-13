e8c0472f7f10f702146f6212b15202d4057ed796
Jonathan Tsai <cat2006house@gmail.com>
2024-06-13 16:54:48 +0800

    new branch for developing advanced search

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ async function fetchNewsApi(apiKey, q, country, source) {
 }
 
 fetchNewsApi("7a9c622267b2438ebcb0890ceeada316");
+
+const searchBar = document.getElementById("searchBar");
+const advancedSearch = document.getElementById("advanced");
+
+advancedSearch.addEventListener("click", function () {
+  // create a form and attach the first input box into it
+
+  const input = document.createElement("div");
+  input.classList.add("asi");
+
+  const qInput = document.createElement("input");
+  qInput.classList.add(
+    "block",
+    "max-w-44",
+    "mx-auto",
+    "p-2.5",
+    "w-full",
+    "z-20",
+    "text-sm",
+    "text-gray-900",
+    "bg-gray-50",
+    "rounded-lg",
+    "border-s-gray-50",
+    "border-s-2",
+    "border",
+    "border-gray-300",
+    "focus:ring-blue-500",
+    "focus:border-blue-500",
+    "dark:bg-gray-700",
+    "dark:border-s-gray-700",
+    "dark:border-gray-600",
+    "dark:placeholder-gray-400",
+    "dark:text-white",
+    "dark:focus:border-blue-500"
+  );
+
+  qInput.setAttribute("type", "keywords");
+  qInput.setAttribute("placeholder", "Keywords");
+  input.appendChild(qInput);
+  searchBar.appendChild(input);
+});
